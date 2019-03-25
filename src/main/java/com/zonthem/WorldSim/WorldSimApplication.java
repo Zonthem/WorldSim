fcbe8b882ebc9c3425cd263d15941da9be0c17ab
Nico_Garaud <nicolas.garaud@sodiaal.fr>
2019-03-25 16:57:29 +0100

    Initial commit

---
+++ src/main/java/com/zonthem/WorldSim/WorldSimApplication.java
@@ -0,0 +1,13 @@
+package com.zonthem.WorldSim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldSimApplication.class, args);
+	}
+
+}
